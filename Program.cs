6306246bb38e3fb2baf0667be6df403d33675d28
Jakub Vrtiška <jkbvrtiska@gmail.com>
2022-06-08 13:43:23 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+var lines = new List<string>();
+using (StreamReader sr = new StreamReader("input.txt"))
+{
+    string line;
+    int count = 0;
+    while ((line = sr.ReadLine()) != null)
+    {
+        string[] parts = line.Split(';');
+
+        for (int i = 0; i < parts.Length; i++)
+        {
+            if (parts[i] == " true")
+            {
+                count++;
+            }
+        }
+        
+    }
+    using (StreamWriter writer = new StreamWriter("output.txt"))
+    {
+        writer.Write(DateTime.Now.ToString() + ";" + count);
+    }
+
+}
